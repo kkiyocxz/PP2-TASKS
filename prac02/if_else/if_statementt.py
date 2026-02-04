bea5fb6fcecf3f7f16b572fb7f84cb84df7575ad
kkiyocxz <lidannavi@gmail.com>
2026-02-05 02:25:21 +0500

    prac02 in progress

---
+++ prac02/if_else/if_statementt.py
@@ -0,0 +1,25 @@
+#if_else the basic
+a=111
+b=123
+if(a>=b):
+    print(a)
+else:
+    print(b)
+#some basic examples
+name=input("enter your name ")
+if name=="Adilzhan":
+    print("Adilzhan")
+else:
+    print("you are not Adilzhan")
+
+x=int(input())
+if(x%10==1):
+    print(x%10)
+else:
+    print("the last number is not 1")
+
+x=input()
+if (not (x=="student")):
+    print("yor are not a student")
+else:
+    print("you are a student")
