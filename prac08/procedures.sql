34ac27f5d4930f44fcadba5bca83e8685cc290e5
kkiyocxz <lidannavi@gmail.com>
2026-04-09 08:31:42 +0500

    s

--- prac08/procedures.sql
+++ prac08/procedures.sql
@@ -1,73 +1,42 @@
--- Procedure 1:
--- insert a new user by name and phone; if the user already exists, update their phone
-
 CREATE OR REPLACE PROCEDURE upsert_contact(p_name VARCHAR, p_phone VARCHAR)
-LANGUAGE plpgsql
-AS $$
+LANGUAGE plpgsql AS $$
 BEGIN
-    IF EXISTS (SELECT 1 FROM contacts WHERE name = p_name) THEN
-        UPDATE contacts
-        SET phone = p_phone
-        WHERE name = p_name;
+    IF EXISTS (SELECT 1 FROM phonebook WHERE first_name = p_name) THEN
+        UPDATE phonebook SET phone = p_phone WHERE first_name = p_name;
     ELSE
-        INSERT INTO contacts(name, phone)
-        VALUES (p_name, p_phone);
+        INSERT INTO phonebook (first_name, phone) VALUES (p_name, p_phone);
     END IF;
 END;
 $$;
 
 
--- Procedure 2:
--- insert many new users from a list of names and phones
--- use a loop and IF inside the procedure
--- validate phone correctness
--- return all incorrect data using NOTICE
-
-CREATE OR REPLACE PROCEDURE insert_many_contacts(
-    p_names TEXT[],
-    p_phones TEXT[]
-)
-LANGUAGE plpgsql
-AS $$
+CREATE OR REPLACE PROCEDURE insert_many_contacts(data JSON)
+LANGUAGE plpgsql AS $$
 DECLARE
-    i INT;
-    invalid_data TEXT := '';
+    item JSON;
+    p_name VARCHAR;
+    p_phone VARCHAR;
 BEGIN
-    IF array_length(p_names, 1) IS DISTINCT FROM array_length(p_phones, 1) THEN
-        RAISE EXCEPTION 'Names and phones arrays must have the same length';
-    END IF;
-
-    FOR i IN 1 .. array_length(p_names, 1) LOOP
-        IF p_phones[i] ~ '^[0-9+() -]+$' THEN
-            IF EXISTS (SELECT 1 FROM contacts WHERE name = p_names[i]) THEN
-                UPDATE contacts
-                SET phone = p_phones[i]
-                WHERE name = p_names[i];
-            ELSE
-                INSERT INTO contacts(name, phone)
-                VALUES (p_names[i], p_phones[i]);
-            END IF;
+    FOR item IN SELECT * FROM json_array_elements(data)
+    LOOP
+        p_name := item->>'name';
+        p_phone := item->>'phone';
+        IF p_phone ~ '^\+?[0-9]{10,15}$' THEN
+            INSERT INTO phonebook (first_name, phone)
+            VALUES (p_name, p_phone)
+            ON CONFLICT (phone) DO NOTHING;
         ELSE
-            invalid_data := invalid_data || '(' || p_names[i] || ', ' || p_phones[i] || '); ';
+            RAISE NOTICE 'Неверный номер для %: %', p_name, p_phone;
         END IF;
     END LOOP;
-
-    IF invalid_data <> '' THEN
-        RAISE NOTICE 'Incorrect data: %', invalid_data;
-    END IF;
 END;
 $$;
 
-
--- Procedure 3:
--- delete data from the table by username or phone
 
 CREATE OR REPLACE PROCEDURE delete_contact(p_value VARCHAR)
-LANGUAGE plpgsql
-AS $$
+LANGUAGE plpgsql AS $$
 BEGIN
-    DELETE FROM contacts
-    WHERE name = p_value OR phone = p_value;
+    DELETE FROM phonebook
+    WHERE first_name = p_value OR phone = p_value;
 END;
 $$;
-
